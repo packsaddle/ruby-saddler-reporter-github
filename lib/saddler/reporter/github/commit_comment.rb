1e079ce7ee126eb2ae39a51a7cc0d2d99e232b1f
sanemat <o.gata.ken@gmail.com>
2015-02-20 02:47:33 +0900

    docs(parse): add parsed example

--- lib/saddler/reporter/github/commit_comment.rb
+++ lib/saddler/reporter/github/commit_comment.rb
@@ -19,6 +19,26 @@ module Saddler
           # create commit_comment
         end
 
+        # {"checkstyle"=>
+        #    {"file"=>
+        #       [{"error"=>
+        #           [{"@column"=>"4",
+        #             "@line"=>"22",
+        #             "@message"=>"Assignment Branch Condition size for report is too high. [34.34/15]",
+        #             "@severity"=>"info",
+        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/AbcSize"},
+        #            {"@column"=>"4",
+        #             "@line"=>"22",
+        #             "@message"=>"Cyclomatic complexity for report is too high. [10/6]",
+        #             "@severity"=>"info",
+        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/CyclomaticComplexity"},
+        #            {"@column"=>"6",
+        #             "@line"=>"39",
+        #             "@message"=>"Use space after control keywords.",
+        #             "@severity"=>"info",
+        #             "@source"=>"com.puppycrawl.tools.checkstyle.Style/SpaceAfterControlKeyword"}],
+        #         "@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/cli.rb"},
+        #        {"@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/reporter.rb"}]}}
         def parse(xml)
           Nori
             .new(parser: :rexml)
