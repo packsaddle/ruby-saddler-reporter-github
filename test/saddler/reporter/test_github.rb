5e75f2da00bedae55e29c8d7a830233934c3d223
sanemat <o.gata.ken@gmail.com>
2015-02-20 02:58:08 +0900

    chore(test): remove fail test

--- test/saddler/reporter/test_github.rb
+++ test/saddler/reporter/test_github.rb
@@ -4,8 +4,4 @@ class Saddler::Reporter::TestGithub < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Saddler::Reporter::Github::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
