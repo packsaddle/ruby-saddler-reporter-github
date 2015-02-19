34d553bdcfd384a4a1edb5842ccbada93c14d284
sanemat <o.gata.ken@gmail.com>
2015-02-19 20:18:39 +0900

    fix(test): change file structures

--- test/saddler/reporter/test_github.rb
+++ test/saddler/reporter/test_github.rb
@@ -1,6 +1,6 @@
 require 'minitest_helper'
 
-class TestSaddler::Reporter::Github < Minitest::Test
+class Saddler::Reporter::TestGithub < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Saddler::Reporter::Github::VERSION
   end
