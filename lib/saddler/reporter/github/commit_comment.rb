30cd4badf4ea61cf36907bb02bf8e1b8d7abbc56
sanemat <o.gata.ken@gmail.com>
2015-02-20 01:27:38 +0900

    docs(comment): add exec sequence

--- lib/saddler/reporter/github/commit_comment.rb
+++ lib/saddler/reporter/github/commit_comment.rb
@@ -8,7 +8,10 @@ module Saddler
 
         # https://developer.github.com/v3/repos/comments/#create-a-commit-comment
         def report(messages, _options)
-
+          # build comment
+          # fetch commit_comments
+          # compare commit_comments.include?(comment)
+          # create commit_comment
         end
       end
     end
