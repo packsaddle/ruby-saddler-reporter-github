5e8a383a40c715f4ddc8761c50abbadabb346c0e
sanemat <o.gata.ken@gmail.com>
2015-02-20 02:29:25 +0900

    feat(comment): build comment with nori

--- lib/saddler/reporter/github/commit_comment.rb
+++ lib/saddler/reporter/github/commit_comment.rb
@@ -7,11 +7,35 @@ module Saddler
         end
 
         # https://developer.github.com/v3/repos/comments/#create-a-commit-comment
-        def report(messages, _options)
-          # build comment
+        def report(messages, options)
+          sha1 = options[:sha1]
+          data = parse(messages)
+
+          comment = build_comment(data)
+
           # fetch commit_comments
           # compare commit_comments.include?(comment)
           # create commit_comment
+        end
+
+        def parse(xml)
+          Nori
+            .new(parser: :rexml)
+            .parse(xml)
+        end
+
+        def build_comment(data)
+          buffer = []
+          files = data['checkstyle']['file'] ||= []
+          files.each do |file|
+            errors = file['error'] ||= []
+            errors.each do |error|
+              severity = error['@severity'] && error['@severity'].upcase
+              message = error['@message']
+              buffer << "#{severity}: #{message}"
+            end
+          end
+          buffer.join("\n")
         end
       end
     end
