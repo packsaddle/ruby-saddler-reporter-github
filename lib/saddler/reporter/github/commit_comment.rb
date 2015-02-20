38f2f2bb41746171aab9f1f14d65c6de9ddef6d5
sanemat <o.gata.ken@gmail.com>
2015-02-20 20:54:04 +0900

    refactor(helper): extend method to module

--- lib/saddler/reporter/github/commit_comment.rb
+++ lib/saddler/reporter/github/commit_comment.rb
@@ -2,9 +2,7 @@ module Saddler
   module Reporter
     module Github
       class CommitComment
-        def initialize(output)
-          @output = output
-        end
+        include Helper
 
         # https://developer.github.com/v3/repos/comments/#create-a-commit-comment
         def report(messages, options)
@@ -26,32 +24,6 @@ module Saddler
           return if commit_comments.include?(comment)
           # create commit_comment
           client.create_commit_comment(comment)
-        end
-
-        # {"checkstyle"=>
-        #    {"file"=>
-        #       [{"error"=>
-        #           [{"@column"=>"4",
-        #             "@line"=>"22",
-        #             "@message"=>"Assignment Branch Condition size for report is too high. [34.34/15]",
-        #             "@severity"=>"info",
-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/AbcSize"},
-        #            {"@column"=>"4",
-        #             "@line"=>"22",
-        #             "@message"=>"Cyclomatic complexity for report is too high. [10/6]",
-        #             "@severity"=>"info",
-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Metrics/CyclomaticComplexity"},
-        #            {"@column"=>"6",
-        #             "@line"=>"39",
-        #             "@message"=>"Use space after control keywords.",
-        #             "@severity"=>"info",
-        #             "@source"=>"com.puppycrawl.tools.checkstyle.Style/SpaceAfterControlKeyword"}],
-        #         "@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/cli.rb"},
-        #        {"@name"=>"/Users/sane/work/ruby-study/saddler/lib/saddler/reporter.rb"}]}}
-        def parse(xml)
-          Nori
-            .new(parser: :rexml)
-            .parse(xml)
         end
 
         def build_body(data)
