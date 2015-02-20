5dbbc2db1843bd0251088008f3546a119fd5da50
sanemat <o.gata.ken@gmail.com>
2015-02-20 22:40:26 +0900

    style(rubocop): fix by rubocop

--- lib/saddler/reporter/github/comment.rb
+++ lib/saddler/reporter/github/comment.rb
@@ -4,8 +4,8 @@ module Saddler
       Comment = Struct.new(:sha, :body, :path, :position) do
         def ==(other)
           position == other.position &&
-            path == other.path &&
-            body == other.body
+          path == other.path &&
+          body == other.body
         end
       end
     end
--- lib/saddler/reporter/github/helper.rb
+++ lib/saddler/reporter/github/helper.rb
@@ -19,4 +19,3 @@ module Saddler
     end
   end
 end
-
