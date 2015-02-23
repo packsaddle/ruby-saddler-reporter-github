d1e31be2a230f4e23f51065419e3e60b300867db
sanemat <o.gata.ken@gmail.com>
2015-02-23 23:49:25 +0900

    refactor(commitreview): use git_diff_parser api

--- lib/saddler/reporter/github/commit_review_comment.rb
+++ lib/saddler/reporter/github/commit_review_comment.rb
@@ -7,35 +7,6 @@ module Saddler
         include Support
         include Helper
 
-        # copy from checkstyle_filter-git
-        def self.file_in_patches?(file_name, patches)
-          patches
-            .map(&:file)
-            .map { |file| Pathname.new(file).expand_path }
-            .include?(Pathname.new(file_name).expand_path)
-        end
-
-        # copy from checkstyle_filter-git
-        def self.file_element_error_line_no_in_patches?(file_name, patches, line_no)
-          diff_patches = patches
-                         .select { |patch| same_file?(patch.file, file_name) }
-          return false if diff_patches.empty?
-
-          modified_lines = Set.new
-          diff_patches.map do |patch|
-            patch.changed_lines.map do |line|
-              modified_lines << line.number
-            end
-          end
-
-          modified_lines.include?(line_no)
-        end
-
-        # copy from checkstyle_filter-git
-        def self.same_file?(one, other)
-          Pathname.new(one).expand_path == Pathname.new(other).expand_path
-        end
-
         # https://developer.github.com/v3/repos/comments/#create-a-commit-comment
         def report(messages, options)
           repo_path = '.'
@@ -65,17 +36,18 @@ module Saddler
           files.each do |file|
             errors = file['error'] ||= []
             file_name = file['@name'] ||= ''
-            diff_patch = patches.detect { |patch| self.class.same_file?(patch.file, file_name) }
-            next unless self.class.file_in_patches?(file_name, patches)
+            patch = patches.find_patch_by_file(file_relative_path_string(file_name))
+            next unless patch
+
             errors.each do |error|
               line_no = error['@line'] && error['@line'].to_i
-              next unless self.class.file_element_error_line_no_in_patches?(file_name, patches, line_no)
+              next unless patch.changed_line_numbers.include?(line_no)
 
               severity = error['@severity'] && error['@severity'].upcase
               message = error['@message']
-              position = diff_patch.changed_lines.detect { |line| line.number == line_no }.patch_position
+              position = patch.changed_lines.detect { |line| line.number == line_no }.patch_position
 
-              comments << Comment.new(sha, [severity, message].compact.join(': '), diff_patch.file, position)
+              comments << Comment.new(sha, [severity, message].compact.join(': '), patch.file, position)
             end
           end
           comments
