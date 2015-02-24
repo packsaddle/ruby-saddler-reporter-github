03332cdf3f492a38b4fc753a62460dbfea4e35fa
sanemat <o.gata.ken@gmail.com>
2015-02-24 14:43:14 +0900

    refactor(pullrequest): arrange method order

--- lib/saddler/reporter/github/pull_request_comment.rb
+++ lib/saddler/reporter/github/pull_request_comment.rb
@@ -7,19 +7,21 @@ module Saddler
 
         # https://developer.github.com/v3/issues/comments/#create-a-comment
         def report(messages, _options)
+          repo_path = '.'
+          repo = GitRepository.new(repo_path)
+
           data = parse(messages)
+
+          client = Client.new(repo)
+          # fetch pull_request_comments(issue)
+          pull_request_comments = client.issue_comments
 
           # build comment
           body = concat_body(data)
           return if body.empty?
           comment = Comment.new(sha1 = nil, body, path = nil, position = nil)
 
-          repo_path = '.'
-          repo = GitRepository.new(repo_path)
-          client = Client.new(repo)
 
-          # fetch pull_request_comments(issue)
-          pull_request_comments = client.issue_comments
           # compare pull_request_comments.include?(comment)
           return if pull_request_comments.include?(comment)
           # create pull_request_comment
