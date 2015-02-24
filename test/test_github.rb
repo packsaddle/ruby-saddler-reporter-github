b4ed1101b25cca4467289e6816eb839886df4816
sanemat <o.gata.ken@gmail.com>
2015-02-24 22:16:17 +0900

    style(test): fix by rubocop

--- test/test_github.rb
+++ test/test_github.rb
@@ -3,13 +3,13 @@ require_relative 'helper'
 module Saddler
   module Reporter
     module Github
-        class TestGithub < Test::Unit::TestCase
-          test 'version' do
-            assert do
-              !::Saddler::Reporter::Github::VERSION.nil?
-            end
+      class TestGithub < Test::Unit::TestCase
+        test 'version' do
+          assert do
+            !::Saddler::Reporter::Github::VERSION.nil?
           end
         end
+      end
     end
   end
 end
