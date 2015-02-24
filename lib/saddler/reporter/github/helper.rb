72343b685132f60e0e8726e2b43202e58414a64b
sanemat <o.gata.ken@gmail.com>
2015-02-24 14:49:05 +0900

    fix(helper): adjust nori pattern

--- lib/saddler/reporter/github/helper.rb
+++ lib/saddler/reporter/github/helper.rb
@@ -5,8 +5,10 @@ module Saddler
         def concat_body(data)
           buffer = []
           files = data['checkstyle']['file'] ||= []
+          files = [files] if files.kind_of?(Hash)
           files.each do |file|
             errors = file['error'] ||= []
+            errors = [file['error']] if errors.kind_of?(Hash)
             errors.each do |error|
               severity = error['@severity'] && error['@severity'].upcase
               message = error['@message']
