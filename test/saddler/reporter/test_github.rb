6b82c2c966bc5aa8a2e31c204f28efb83db2d0a1
sanemat <o.gata.ken@gmail.com>
2015-02-24 22:14:18 +0900

    test(default): use test-unit

--- test/saddler/reporter/test_github.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest_helper'
-
-class Saddler::Reporter::TestGithub < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Saddler::Reporter::Github::VERSION
-  end
-end
---
+++ test/test_github.rb
@@ -0,0 +1,15 @@
+require_relative 'helper'
+
+module Saddler
+  module Reporter
+    module Github
+        class TestGithub < Test::Unit::TestCase
+          test 'version' do
+            assert do
+              !::Saddler::Reporter::Github::VERSION.nil?
+            end
+          end
+        end
+    end
+  end
+end
